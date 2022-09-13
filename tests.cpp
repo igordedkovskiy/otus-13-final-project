aee3c7c90cbb1026ce90d24986e76dbf9591bcef
Igor Dedkovskiy <divmy@yandex.ru>
2022-09-13 16:41:36 +0300

    Update test: unfinished

--- tests.cpp
+++ tests.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <list>
 #include <map>
 #include <fstream>
 #include <sstream>
@@ -6,6 +7,7 @@
 #include <cassert>
 #include <numeric>
 #include <iterator>
+#include <chrono>
 #include "gtest/gtest.h"
 
 #include "Queue.hpp"
@@ -50,8 +52,91 @@ TEST(TEST_QUEUE, serialize_queue)
     }
 }
 
+/// \brief Compare execution time in two cases. First is one producer, one consumer.
+///        Second is multiple producers, multiple consumers.
 TEST(TEST_QUEUE, producer_consumer)
 {
+    using namespace std::chrono;
+    using namespace threadsafe_containers;
+    using clock = system_clock;
+    using data_t = std::uint64_t;
+    using data_collection_t = std::list<data_t>;
+    using all_data_collection_t = std::vector<data_collection_t>;
+
+    constexpr std::size_t num_of_elements {1000};
+
+    auto make_data = [num_of_elements](auto num_of_producers)
+    {
+        return all_data_collection_t(num_of_producers,
+                                     data_collection_t(num_of_elements/num_of_producers,1));
+    };
+
+    auto producer = [](Queue<data_t>& queue, data_collection_t& data)
+    {
+        std::cout << "producer " << std::this_thread::get_id() << ": start" << std::endl;
+        for(auto it {std::begin(data)}; it != std::end(data);)
+        {
+            queue.wait_if_full_push(*it);
+            it = data.erase(it);
+        }
+        std::cout << "producer " << std::this_thread::get_id() << ": end" << std::endl;
+    };
+
+    auto consumer = [](Queue<data_t>& queue, const all_data_collection_t& data)
+    {
+        std::cout << "consumer " << std::this_thread::get_id() << ": start" << std::endl;
+        auto data_empty = [&data]()
+        {
+            for(const auto& el:data)
+            {
+                if(!el.empty())
+                    return false;
+            }
+            return true;
+        };
+
+        std::size_t cntr = 0;
+
+        do
+        {
+            auto el {queue.pop()};
+            if(el)
+                cntr += *el;
+        }
+        while(!queue.empty() || !data_empty());
+        std::cout << "consumer " << std::this_thread::get_id() << ": end" << std::endl;
+    };
+
+    auto run = [&producer, &consumer, &make_data](auto num_of_producers, auto num_of_consumers)
+    {
+        std::cout << "run start" << std::endl;
+        auto data {make_data(num_of_producers)};
+        Queue<data_t> queue;
+
+        std::vector<std::thread> producers;
+        producers.reserve(num_of_producers);
+        for(auto& producers_data:data)
+            producers.emplace_back(producer, std::ref(queue), std::ref(producers_data));
+
+        std::vector<std::thread> consumers;
+        consumers.reserve(num_of_consumers);
+        for(std::size_t cntr {0}; cntr < num_of_consumers; ++cntr)
+            consumers.emplace_back(consumer, std::ref(queue), std::ref(data));
+
+        for(auto& cons:consumers)
+        {
+            if(cons.joinable())
+                cons.join();
+        }
+        for(auto& prod:producers)
+        {
+            if(prod.joinable())
+                prod.join();
+        }
+        std::cout << "run end" << std::endl;
+    };
+
+    auto start {clock::now()};
     // create input data
     // create producer
     // move input data into producer
@@ -60,6 +145,16 @@ TEST(TEST_QUEUE, producer_consumer)
     // create framework
     // move producer and consumer into framework
     // run framework
+    run(1, 1);
+    const auto single_duration {duration_cast<milliseconds>(clock::now() - start).count()};
+    start = clock::now();
+    // repeat for multiple tests
+    run(4, 4);
+    const auto multiple_duration {duration_cast<milliseconds>(clock::now() - start).count()};
+    std::cout << "single_duration|multiple_duration (ms): "
+              << single_duration << '|'
+              << multiple_duration << std::endl;
+    EXPECT_LT(single_duration, multiple_duration);
 }
 
 int main(int argc, char** argv)
