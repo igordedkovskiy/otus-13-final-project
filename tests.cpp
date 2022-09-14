faddde4ee4ec9e957e2da934c114c9e9e9f46fa5
Igor Dedkovskiy <divmy@yandex.ru>
2022-09-14 12:13:30 +0300

    Cut test off: temporarily

--- tests.cpp
+++ tests.cpp
@@ -54,134 +54,134 @@ TEST(TEST_QUEUE, serialize_queue)
 
 /// \brief Compare execution time in two cases. First is one producer, one consumer.
 ///        Second is multiple producers, multiple consumers.
-TEST(TEST_QUEUE, producer_consumer)
-{
-    const auto num_of_cores {std::thread::hardware_concurrency()};
-    std::cout << "number of cores is " << num_of_cores << std::endl;
-    if(num_of_cores == 1)
-        return;
+//TEST(TEST_QUEUE, producer_consumer)
+//{
+//    const auto num_of_cores {std::thread::hardware_concurrency()};
+//    std::cout << "number of cores is " << num_of_cores << std::endl;
+//    if(num_of_cores == 1)
+//        return;
 
-    using namespace std::chrono;
-    using namespace threadsafe_containers;
-    using clock = system_clock;
-    using data_t = std::uint64_t;
-    using data_collection_t = std::vector<data_t>;
-    using all_data_collection_t = std::vector<data_collection_t>;
-    using cond_cntr_t = std::atomic_llong;
-    using num_of_elements_t = std::size_t;
-    using ranges_t = std::vector<num_of_elements_t>;
+//    using namespace std::chrono;
+//    using namespace threadsafe_containers;
+//    using clock = system_clock;
+//    using data_t = std::uint64_t;
+//    using data_collection_t = std::vector<data_t>;
+//    using all_data_collection_t = std::vector<data_collection_t>;
+//    using cond_cntr_t = std::atomic_llong;
+//    using num_of_elements_t = std::size_t;
+//    using ranges_t = std::vector<num_of_elements_t>;
 
-    auto make_data = [](std::size_t num_of_producers, const ranges_t& ranges)
-    {
-        all_data_collection_t data;
-        data.reserve(num_of_producers);
-        for(auto num_of_elements:ranges)
-            data.emplace_back(data_collection_t(num_of_elements, 1));
-        return data;
-    };
+//    auto make_data = [](std::size_t num_of_producers, const ranges_t& ranges)
+//    {
+//        all_data_collection_t data;
+//        data.reserve(num_of_producers);
+//        for(auto num_of_elements:ranges)
+//            data.emplace_back(data_collection_t(num_of_elements, 1));
+//        return data;
+//    };
 
-    auto producer = [](Queue<data_t>& queue, const data_collection_t& data, cond_cntr_t& el_left)
-    {
-        for(auto el:data)
-        {
-            queue.wait_if_full_push(el);
-            --el_left;
-        }
-    };
+//    auto producer = [](Queue<data_t>& queue, const data_collection_t& data, cond_cntr_t& el_left)
+//    {
+//        for(auto el:data)
+//        {
+//            queue.wait_if_full_push(el);
+//            --el_left;
+//        }
+//    };
 
-    auto consumer = [](Queue<data_t>& queue, const cond_cntr_t& el_left)
-    {
-        std::size_t cntr {0};
-        while(!queue.empty() || el_left)
-        {
-            auto el {queue.pop()};
-            if(el)
-            {
-                cntr += *el;
-                using namespace std::chrono_literals;
-                std::this_thread::sleep_for(1ms);
-            }
-        }
-    };
+//    auto consumer = [](Queue<data_t>& queue, const cond_cntr_t& el_left)
+//    {
+//        std::size_t cntr {0};
+//        while(!queue.empty() || el_left)
+//        {
+//            auto el {queue.pop()};
+//            if(el)
+//            {
+//                cntr += *el;
+//                using namespace std::chrono_literals;
+//                std::this_thread::sleep_for(1ms);
+//            }
+//        }
+//    };
 
-    // create input data
-    // create producer
-    // move input data into producer
-    // create graph
-    // create consumer, move graph into consumer
-    // create framework
-    // move producer and consumer into framework
-    // run framework
-    auto run = [&producer, &consumer, &make_data]
-            (std::size_t num_of_producers, std::size_t num_of_consumers, std::size_t num_of_elements)
-    {
-        const auto start {clock::now()};
+//    // create input data
+//    // create producer
+//    // move input data into producer
+//    // create graph
+//    // create consumer, move graph into consumer
+//    // create framework
+//    // move producer and consumer into framework
+//    // run framework
+//    auto run = [&producer, &consumer, &make_data]
+//            (std::size_t num_of_producers, std::size_t num_of_consumers, std::size_t num_of_elements)
+//    {
+//        const auto start {clock::now()};
 
-        auto split = [&num_of_elements, &num_of_producers]()
-        {
-            if(num_of_producers == 1)
-                return ranges_t(1, num_of_elements);
-            const auto size {num_of_elements / num_of_producers};
-            ranges_t ranges;
-            ranges.reserve(num_of_producers);
-            for(std::size_t cntr {0}; cntr < num_of_producers - 1; ++cntr)
-                ranges.push_back(size);
-            ranges.push_back(num_of_elements - size * (num_of_producers - 1));
-            return ranges;
-        };
+//        auto split = [&num_of_elements, &num_of_producers]()
+//        {
+//            if(num_of_producers == 1)
+//                return ranges_t(1, num_of_elements);
+//            const auto size {num_of_elements / num_of_producers};
+//            ranges_t ranges;
+//            ranges.reserve(num_of_producers);
+//            for(std::size_t cntr {0}; cntr < num_of_producers - 1; ++cntr)
+//                ranges.push_back(size);
+//            ranges.push_back(num_of_elements - size * (num_of_producers - 1));
+//            return ranges;
+//        };
 
-        const auto ranges {split()};
-//        std::cout << "ranges: ";
-//        for(auto v:ranges)
-//            std::cout << v << ' ';
-//        std::cout << std::endl;
-        const auto data {make_data(num_of_producers, ranges)};
-        Queue<data_t> queue;
+//        const auto ranges {split()};
+////        std::cout << "ranges: ";
+////        for(auto v:ranges)
+////            std::cout << v << ' ';
+////        std::cout << std::endl;
+//        const auto data {make_data(num_of_producers, ranges)};
+//        Queue<data_t> queue;
 
-        cond_cntr_t elements_left (num_of_elements);
+//        cond_cntr_t elements_left (num_of_elements);
 
-        std::vector<std::thread> producers;
-        producers.reserve(num_of_producers);
-        for(auto& producers_data:data)
-            producers.emplace_back(producer, std::ref(queue), std::ref(producers_data), std::ref(elements_left));
+//        std::vector<std::thread> producers;
+//        producers.reserve(num_of_producers);
+//        for(auto& producers_data:data)
+//            producers.emplace_back(producer, std::ref(queue), std::ref(producers_data), std::ref(elements_left));
 
-        std::vector<std::thread> consumers;
-        consumers.reserve(num_of_consumers);
-        for(std::size_t cntr {0}; cntr < num_of_consumers; ++cntr)
-            consumers.emplace_back(consumer, std::ref(queue), std::ref(elements_left));
+//        std::vector<std::thread> consumers;
+//        consumers.reserve(num_of_consumers);
+//        for(std::size_t cntr {0}; cntr < num_of_consumers; ++cntr)
+//            consumers.emplace_back(consumer, std::ref(queue), std::ref(elements_left));
 
-        for(auto& cons:consumers)
-        {
-            if(cons.joinable())
-                cons.join();
-        }
-        for(auto& prod:producers)
-        {
-            if(prod.joinable())
-                prod.join();
-        }
-        return std::make_pair(duration_cast<milliseconds>(clock::now() - start).count(), queue.empty());
-    };
+//        for(auto& cons:consumers)
+//        {
+//            if(cons.joinable())
+//                cons.join();
+//        }
+//        for(auto& prod:producers)
+//        {
+//            if(prod.joinable())
+//                prod.join();
+//        }
+//        return std::make_pair(duration_cast<milliseconds>(clock::now() - start).count(), queue.empty());
+//    };
 
-    constexpr std::size_t num_of_elements {1000};
-    /// \note one producers / consumers
-    const auto [single_duration, result] {run(1, 1, num_of_elements)};
-    ASSERT_TRUE(result);
+//    constexpr std::size_t num_of_elements {1000};
+//    /// \note one producers / consumers
+//    const auto [single_duration, result] {run(1, 1, num_of_elements)};
+//    ASSERT_TRUE(result);
 
-    for(std::size_t mfactor {1}; mfactor < 6; ++mfactor)
-    {
-        /// \note multiple producers / consumers
-        /// \note num_of_producers = num_of_cores * mfactor
-        const auto [multiple_duration, result2] {run(num_of_cores * mfactor, num_of_cores * mfactor,
-                                          num_of_elements)};
-        ASSERT_TRUE(result2);
+//    for(std::size_t mfactor {1}; mfactor < 6; ++mfactor)
+//    {
+//        /// \note multiple producers / consumers
+//        /// \note num_of_producers = num_of_cores * mfactor
+//        const auto [multiple_duration, result2] {run(num_of_cores * mfactor, num_of_cores * mfactor,
+//                                          num_of_elements)};
+//        ASSERT_TRUE(result2);
 
-        std::cout << "single_duration|multiple_duration (ms): "
-                  << single_duration << '|'
-                  << multiple_duration << std::endl;
-        ASSERT_GE(single_duration, multiple_duration);
-    }
-}
+//        std::cout << "single_duration|multiple_duration (ms): "
+//                  << single_duration << '|'
+//                  << multiple_duration << std::endl;
+//        ASSERT_GE(single_duration, multiple_duration);
+//    }
+//}
 
 int main(int argc, char** argv)
 {
